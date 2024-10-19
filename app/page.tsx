2f37cc38195a4dd2a8d22fce724ddc49511f36b9
Adrianosky <kienadr@gmail.com>
2024-10-19 11:23:42 +0200

    fix button in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+'use client'
 import Image from "next/image";
 import Button from "@/components/Button";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+  
+  const handleClick = () => {
+    router.push('/menu');
+  };
+
   return (
     <div className="w-full px-10 sm: flex justify-center items-center">
       <div className="flex flex-col w-full justify-center items-center pt-[90px] sm:max-w-[1000px]">
-        <h1 className="text-white text-[30px] sm:text-[64px] text-center pt-[60px]">
+        <h1 className="pt-[50px] text-white text-[30px] sm:text-[64px] text-center sm:pt-[35px]">
           Każdy kawałek sushi robimy z sercem i pasją!
         </h1>
         <div className="flex flex-row w-full justify-center">
@@ -15,7 +23,7 @@ export default function Home() {
         <p className="text-white text-[30px] sm:text-[40px] text-center">
           Składniki najwyższej jakości. Uczta dla oczu. Nieziemski smak.
         </p>
-        <Button text="Zamów teraz" className="mt-6"/>
+        <Button text="Zamów teraz" className="mt-6" onClick={handleClick}/>
       </div>
     </div>
   );
