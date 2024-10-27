fc55eab4b51c5573482f530eaca2e9bcb030fd78
adrian.gasek <adrian.gasek.99@gmail.com>
2024-10-27 11:48:54 +0100

    fix menu, change data menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React, { useEffect, useState, useMemo } from "react";
-import { FaLeaf, FaPepperHot, FaFire } from "react-icons/fa6";
+import { FaLeaf, FaPepperHot, FaFire, FaTruck } from "react-icons/fa6";
 
 interface MenuItem {
   title: string;
   price: string;
+  items?: string[];
+  qty?: string;
 }
 
 interface MenuCategory {
@@ -61,10 +63,14 @@ const MenuComponent = () => {
     <div className="w-full px-10 sm:flex justify-center items-start py-20 bg-black">
       {/* Categories for mobile screens (at the top) */}
       <div className="block sm:hidden text-white mb-8">
-        <h2 className="text-3xl font-bold mb-6">KATEGORIE</h2>
+        <h2 className="text-[35px] font-bold mb-6">KATEGORIE</h2>
+        <div className="flex flex-rown items-center justify-center p-1 border-2 border-green-400 text-[35px] rounded-lg mb-5">
+          <FaTruck className="text-green-400" />
+          <p className="font-bold text-green-400 ml-2">Dostawa za darmo!</p>
+        </div>
         <ul className="flex flex-wrap justify-start">
           <li
-            className={`text-3xl mr-4 mb-2 cursor-pointer ${
+            className={`text-[35px] mr-4 mb-2 cursor-pointer ${
               selectedCategory === "all"
                 ? "text-green-400 underline underline-offset-8"
                 : ""
@@ -76,7 +82,7 @@ const MenuComponent = () => {
           {categories.map((category) => (
             <li
               key={category.id}
-              className={`text-3xl mr-4 mb-2 cursor-pointer ${
+              className={`text-[35px] mr-4 mb-2 cursor-pointer ${
                 selectedCategory === category.category
                   ? "text-green-400 underline underline-offset-8"
                   : ""
@@ -92,13 +98,35 @@ const MenuComponent = () => {
       {/* Menu items */}
       <div className="sm:w-1/2 text-white flex flex-col w-full items-center h-full max-h-[360px] overflow-y-auto sm:max-h-[600px]">
         {filteredMenu.map((category, index) => (
-          <div key={index} className="mb-8 w-full sm:max-w-[400px] sm:w-full sm:flex flex-col">
-            <h3 className="text-[30px] font-semibold mb-4">{category.title}</h3>
+          <div
+            key={index}
+            className="mb-8 w-full sm:max-w-[400px] sm:w-full sm:flex flex-col"
+          >
+            <h3 className="text-[35px] font-semibold mb-4">{category.title}</h3>
             <ul>
               {category.items.map((item, idx) => (
-                <li key={idx} className="flex justify-between mb-2 text-[30px]">
-                  <span>{item.title}</span>
-                  <span>{item.price}</span>
+                <li
+                  key={idx}
+                  className="flex flex-col justify-between mb-2 text-[35px]"
+                >
+                  <div className="flex justify-between w-full">
+                    <span>
+                      <span>{item.title}</span>
+                      {item.qty && (
+                        <span className="ml-2 font-bold">{item.qty}</span>
+                      )}
+                    </span>
+                    <span className="font-bold">{item.price}zł</span>
+                  </div>
+                  {item.items && item.items.length > 0 && (
+                    <ul className="ml-4 mt-1">
+                      {item.items.map((el, index) => (
+                        <li key={index} className="text-[30px]">
+                          {el}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </li>
               ))}
             </ul>
@@ -134,6 +162,10 @@ const MenuComponent = () => {
             </li>
           ))}
         </ul>
+        <div className="flex flex-rown items-center justify-center p-2 text-xl mt-8">
+          <FaTruck className="text-green-400" />
+          <p className="font-bold text-green-400 ml-2">Dostawa za darmo!</p>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Button from "@/components/Button";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  
+
   const handleClick = () => {
-    router.push('/menu');
+    router.push("/menu");
   };
 
   return (
@@ -17,13 +17,21 @@ export default function Home() {
           Każdy kawałek sushi robimy z sercem i pasją!
         </h1>
         <div className="flex flex-row w-full justify-center">
-          <p className="opacity-40 text-white text-[64px] leading-2 sm:text-[106px] tracking-tight">寿<br/>司</p>
-          <Image src="/img/homeImage.png" width={500} height={500} alt="rolki sushi" className="sm: max-h-[300px] object-contain"/>
+          <p className="opacity-40 text-white text-[64px] leading-2 sm:text-[106px] tracking-tight">
+            寿<br />司
+          </p>
+          <Image
+            src="/img/homeImage.png"
+            width={500}
+            height={500}
+            alt="rolki sushi"
+            className="sm: max-h-[300px] object-contain"
+          />
         </div>
         <p className="text-white text-[30px] sm:text-[40px] text-center">
           Składniki najwyższej jakości. Uczta dla oczu. Nieziemski smak.
         </p>
-        <Button text="Zamów teraz" className="mt-6" onClick={handleClick}/>
+        <Button text="Zobacz menu" className="mt-6" onClick={handleClick} />
       </div>
     </div>
   );
